bc7bc64a1fa1231f3ff29653a4732b670e2b7a61
Manos Pappas <mano.pappa@gmail.com>
2013-06-10 21:49:50 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Final Project')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Manos Pappas Webtool App Final Project | Home")
+                        :text => "Manos Pappas Webtool App Final Project")
+    end
+	
+	it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
