2b2d47f5e84812b453596e4e099cb6053841b988
Manos Pappas <mano.pappa@gmail.com>
2013-06-10 14:29:05 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PappasFinalApp::Application.config.secret_token = '05e2b47d452a626a9b1b13ca49f074eb8d08b4fe4fbb2f2c081b5ae9e18b404741f75aa7be23dd9e9a50e0301c20d1700a2279c339eee8c9649920a1698b6b0a'
