9e856eb8ea0b6028304b1a69377ffba4f61e87d8
Manos Pappas <mano.pappa@gmail.com>
2013-06-11 20:29:56 +0300

    Added layout links (with routes)

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'bootstrap-sass', '2.1'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-spork', '1.2.0'
+  gem 'childprocess', '0.3.9'
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,47 +6,61 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'Final Project'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Final Project')
+      visit root_path
+      page.should have_selector('h1', text: 'Final Project')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
-                        :text => "Manos Pappas Webtool App Final Project")
+                        text: "Manos Pappas Webtool App Final Project")
     end
 	
 	it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
-                        :text => "Manos Pappas Webtool App Final Project | Help")
+                        text: "Manos Pappas Webtool App Final Project | Help")
     end
   end
 
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
-                    :text => "Manos Pappas Webtool App Final Project | About Us")
+                    text: "Manos Pappas Webtool App Final Project | About Us")
     end
   end
-end
+  
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Manos Pappas Webtool App Final Project | Contact")
+    end
+  end
+  end
