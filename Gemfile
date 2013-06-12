88e7b45f17af22d7a3b54866965281cf7839d84e
Manos Pappas <mano.pappa@gmail.com>
2013-06-13 01:26:18 +0300

    Added a User factory and test for the show page

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :test do
   gem 'rb-fchange', '0.0.5'
   gem 'rb-notifu', '0.0.4'
   gem 'win32console', '1.3.2'
+  gem 'factory_girl_rails', '4.1.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Manos Pappas"
+    email    "manos@gmail.gr"
+    password "pappas"
+    password_confirmation "pappas"
+  end
+end
